46082bab294d4e1e81fb649f45bb197dfcd9d049
Gleb8Bask <gleb.baskhanov@gmail.com>
2024-07-13 23:57:50 +0300

    commands

--- bot/bot.py
+++ bot/bot.py
@@ -3,6 +3,7 @@ import os
 import logging
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import message_texts
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -21,13 +22,24 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         return
     await context.bot.send_message(
         chat_id=effective_chat.id,
-        text="Привет! Хелоу! Бонжур!")
+        text=message_texts.GREETINGS)
 
+async def summarize(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    effective_chat = update.effective_chat
+    if not effective_chat:
+        logger.warning("effective_chat is None")
+        return
+    await context.bot.send_message(
+        chat_id=effective_chat.id,
+        text=message_texts.SUMMARIZE)
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
 
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+
+    summarize_handler = CommandHandler('summarize', summarize)
+    application.add_handler(summarize_handler)
 
     application.run_polling()
